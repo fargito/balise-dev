a8a3fc3c9ec58c38c7555d8334454f41bbea6833
fargito <fr.farge96@gmail.com>
2017-12-19 14:42:19 +0100

    changed admin filters 2

--- subventions/admin.py
+++ subventions/admin.py
@@ -3,7 +3,6 @@ from .models import TypeSubvention, VagueSubventions, Subvention
 
 class TypeSubventionAdmin(admin.ModelAdmin):
 	"""affichage dans l'interface admin"""
-	list_filter = ('nom',)
 	search_fields = ('nom',)
 
 
@@ -12,14 +11,14 @@ class VagueSubventionsAdmin(admin.ModelAdmin):
 	list_display = ('type_subvention', 'annee',)
 	list_filter = ('type_subvention', 'annee',)
 	ordering = ('-annee',)
-	search_fields = ('type_subvention', 'annee',)
+	search_fields = ('type_subvention__nom', 'annee',)
 
 
 class SubventionAdmin(admin.ModelAdmin):
 	list_display = ('vague', 'mandat', 'demande', 'accorde', 'postes',)
-	list_filter = ('vague', 'mandat', 'demande', 'accorde', 'postes',)
+	list_filter = ('vague__annee', 'vague__type_subvention', 'mandat__promotion', 'mandat__binet',)
 	ordering = ('vague', 'mandat',)
-	search_fields = ('vague', 'mandat', 'demande', 'accorde', 'postes',)
+	search_fields = ('vague__type_subvention__nom', 'mandat__binet__nom', 'demande', 'accorde', 'postes',)
 
 
 admin.site.register(TypeSubvention, TypeSubventionAdmin)
