fb4fb074dad9cb78757022cea700ae8d1dc443b6
fargito <fr.farge96@gmail.com>
2017-12-25 13:14:41 +0100

    first version of the help module with pdfs but no models yet

--- help/urls.py
+++ help/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-	url(r'^$', views.help_home),
-
+	url(r'^$', views.help_home, name='help_home'),
+	url(r'^(?P<filename>[\w\_]+)/$', views.open_file),
 ]
--- help/views.py
+++ help/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
+from django.http import FileResponse, Http404
 
 
 @login_required
 def help_home(request):
 	"""home to the help module"""
-	try:
-		attributes = request.session['attributes']
-	except:
-		attributes = "pas d'attributes"
 	return render(request, 'help/help_home.html', locals())
+
+
+@login_required
+def open_file(request, filename):
+	"""permet d'ouvrir via leur nom tous les fichiers pdf contenus dans help/guides"""
+	path = "help/guides/" + filename + ".pdf"
+
+	try:
+		return FileResponse(open(path, 'rb'), content_type='application/pdf')
+	except FileNotFoundError:
+		raise Http404()
